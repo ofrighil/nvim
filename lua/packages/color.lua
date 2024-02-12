393461f47256e2ec7eca34079b2c18d62135a97d
ofrighil <eugene.ofriel@gmail.com>
2024-02-11 20:00:55 -0500

    format

--- lua/packages/color.lua
+++ lua/packages/color.lua
@@ -1,32 +1,42 @@
---return {
---  "rebelot/kanagawa.nvim",
---  config = function()
---    require("kanagawa").setup({
---      compile = false,
---      undercurl = true,
---      commentStyle = { italic = false, },
---      functionStyle = {},
---      keywordStyle = { italic = false },
---      statementStyle = { bold = true, },
---      typeStyle = {},
---      transparent = true,
---      dimInactive = false,
---      terminalColors = true,
---      theme = "wave",
---      background = {
---        dark = "wave",
---        light = "lotus",
---      },
---    })
+-- return {
+--     "rebelot/kanagawa.nvim",
+--     config = function()
+--         require("kanagawa").setup({
+--             compile = false,
+--             undercurl = true,
+--             commentStyle = { italic = false, },
+--             functionStyle = {},
+--             keywordStyle = { italic = false },
+--             statementStyle = { bold = true, },
+--             typeStyle = {},
+--             transparent = true,
+--             dimInactive = false,
+--             terminalColors = true,
+--             theme = "wave",
+--             background = {
+--                 dark = "wave",
+--                 light = "lotus",
+--             },
+--         })
 --
---    vim.cmd("colorscheme kanagawa")
---  end
---}
+--         vim.cmd("colorscheme kanagawa")
+--     end
+-- }
+
+-- return {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.cmd("colorscheme tokyonight-moon")
+--     end
+-- }
+
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme tokyonight-moon")
-  end
+    "mcchrish/zenbones.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function ()
+        vim.cmd("colorscheme forestbones")
+        vim.opt.background = 'dark'
+    end
 }
