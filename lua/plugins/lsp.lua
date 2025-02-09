f7a769f16e02bd6bad129102e00a4a57af694562
ofrighil <32347630+ofrighil@users.noreply.github.com>
2025-02-09 13:14:42 -0500

    add jump to defs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,10 +17,28 @@ return {
   },
   config = function()
     local lsp = require("lspconfig")
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
 
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
     lsp.lua_ls.setup({ capabilities = capabilities })
     lsp.nil_ls.setup({ capabilities = capabilities })
     lsp.pyright.setup({ capabilities = capabilities })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then
+          return
+        end
+
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+
+        if client.server_capabilities.inlayHintProvider then
+          vim.keymap.set("n", "gih", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+          end)
+        end
+      end,
+    })
   end,
 }
