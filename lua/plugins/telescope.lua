ea54fddd64ffbef5d53b79c5ee8cc3d77a1c07cd
ofrighil <32347630+ofrighil@users.noreply.github.com>
2026-01-26 11:44:23 -0500

    update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,11 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   opts = {
-    pickers = { find_files = { theme = "ivy" } },
+    defaults = { wrap_results = true },
+    pickers = {
+      find_files = { theme = "ivy" },
+      builtin = { theme = "dropdown" },
+    },
     extensions = { fzf = {} },
   },
   config = function(_, opts)
@@ -18,10 +22,30 @@ return {
 
     vim.keymap.set("n", "<leader>ff", builtin.find_files)
     vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+    vim.keymap.set("n", "<leader>ft", builtin.builtin)
+    vim.keymap.set("n", "<leader>f/", builtin.current_buffer_fuzzy_find)
+
+    vim.keymap.set("n", "<leader>fw", function()
+      builtin.grep_string({ search = vim.fn.expand("<cword>") })
+    end)
+    vim.keymap.set("n", "<leader>fW", function()
+      builtin.grep_string({ search = vim.fn.expand("<cWORD>") })
+    end)
+
     vim.keymap.set("n", "<leader>fh", builtin.help_tags)
 
     vim.keymap.set("n", "<leader>en", function()
       builtin.find_files({ cwd = vim.fn.stdpath("config") })
     end)
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "TelescopePreviewerLoaded",
+      callback = function(args)
+        vim.wo.wrap = true
+        if args.data.filetype ~= "help" then
+          vim.wo.number = true
+        end
+      end,
+    })
   end,
 }
